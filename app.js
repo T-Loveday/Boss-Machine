94c4fc9f8f6a33939637b9d346ef3c519c6d4c8f
Loveday99 <Trevor.Loveday99@Gmail.com>
2025-09-09 16:01:03 -0400

    Server Boilerplate

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
 module.exports = app;
+const PORT = process.env.PORT || 4001;
 
+// Start the server listening on the provided PORT
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 // Add middleware for handling CORS requests from index.html
 
 
@@ -11,5 +17,5 @@ module.exports = app;
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
-
+app.use('/api', apiRouter);
 
